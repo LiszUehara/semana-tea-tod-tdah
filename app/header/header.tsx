b4f362cedecde42c0f175fb7c49e31fe1c32b290
LiszUehara <ueharalisandra@gmail.com>
2025-06-10 12:33:37 -0300

    feat: adicionado primeira versao do header

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,4 +1,21 @@
 
-export function Header() {
-  return <div>Welcome!</div>;
+export default function Header() {
+   return (
+        <section className="flex items-center justify-between p-4 bg-blue-600 text-white mx-auto">
+            <div>
+              <button className="btn btn-primary p-4">
+                <img src="/public/unifamec-logo.webp" alt="Logo" className="h-16 w-auto" />
+              </button>
+  
+            </div>
+            <div className="flex items-center justify-between">
+              <button className="text-lg font-semibold text-color-white p-4 hover:bg-sky-700">Home</button>
+              <button className="text-lg font-semibold text-color-white p-4 hover:bg-sky-700">Sobre</button>
+              <button className="text-lg font-semibold text-color-white p-4 hover:bg-sky-700">Palestrantes</button>
+              <button className="text-lg font-semibold text-color-white p-4 hover:bg-sky-700">FAQ</button>
+              <button className="btn p-4 hover:bg-sky-700 border-4 border-red-500 shadow-lg shadow-cyan-500/50 rounded-none">Login</button>
+              <button className="btn p-4 hover:bg-sky-700 border-4 border-red-900 shadow-lg shadow-cyan-500/50 rounded-none">Passaporte</button>
+            </div>
+        </section>
+   );
 }
