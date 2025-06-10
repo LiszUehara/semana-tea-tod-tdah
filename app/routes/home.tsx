af94faf3ecd4b67981c51cca39e3c35f418ffce6
LiszUehara <ueharalisandra@gmail.com>
2025-06-10 11:28:57 -0300

    feat: config ambiente

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Header } from "../header/header";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Semana " },
+    { name: "description", content: "Semana de TDAH" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Header />;
 }
