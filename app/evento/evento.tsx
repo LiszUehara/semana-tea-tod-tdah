2f809a746b63b061e35710bbecbf2f0cb436c2cc
Lisandra <ueharalisandra@gmail.com>
2025-07-01 12:30:51 -0300

    style: mudanca de estilo na area sobre

--- app/evento/evento.tsx
+++ app/evento/evento.tsx
@@ -2,27 +2,28 @@ import { Pin } from 'lucide-react';
 
 export default function Evento() {
   return (
-    <section id="aboutus"
-      style={{ backgroundImage: "url('/padre-cicero.png')" }}
-      className="flex flex-col items-start justify-center p-8 bg-cover bg-center min-h-screen"
-    >
-      <h1 className="text-5xl font-extrabold text-yellow-400 mb-6 drop-shadow-md">
+    <section
+        id="aboutus"
+        style={{ backgroundImage: "url('/banners-fundo-CCSI_04.jpg')" }}
+        className="flex flex-col items-start justify-center p-8 bg-cover bg-center min-h-screen"
+>
+ <h1 className="text-5xl font-extrabold text-orange-400 mb-6 drop-shadow-md font-arsenica">
         O que esperar do CCSI 2025?
       </h1>
       
-      <p className="text-lg text-black p-6 rounded-xl max-w-3xl leading-relaxed shadow-lg bg-">
-        O <strong>I Congresso Caririense de Saúde Integrada (CCSI)</strong> será um espaço de encontro entre ciência, prática profissional e identidade regional, reunindo estudantes, especialistas e profissionais das áreas da <strong>saúde, educação e assistência social.</strong>
+      <p className="text-lg text-white p-6 rounded-xl max-w-3xl leading-relaxed shadow-lg font-raleway">
+        O <strong className="text-orange-400 ">I Congresso Caririense de Saúde Integrada (CCSI)</strong > será um espaço de encontro entre ciência, prática profissional e identidade regional, reunindo estudantes, especialistas e profissionais das áreas da <strong className="text-orange-400 ">saúde, educação e assistência social.</strong>
         <br />
         <br />
-        Com foco nos transtornos do neurodesenvolvimento — <strong>TDAH, TOD e TEA</strong> — o evento oferecerá palestras, rodas de conversa, oficinas práticas e mesas interdisciplinares que abordam desde os aspectos clínicos e pedagógicos até as políticas públicas e vivências familiares.
+        Com foco nos transtornos do neurodesenvolvimento — <strong className="text-orange-400 ">TDAH, TOD e TEA</strong> — o evento oferecerá palestras, rodas de conversa, oficinas práticas e mesas interdisciplinares que abordam desde os aspectos clínicos e pedagógicos até as políticas públicas e vivências familiares.
         <br />
         <br />
-        📍 Realizado no coração do Cariri cearense, no coração do Cariri cearense, o CCSI 2025 também valoriza o território, a cultura local e os desafios específicos da nossa região, promovendo uma formação que vai além do conteúdo técnico: <strong>integra conhecimento, empatia e compromisso social.</strong>
+        📍 Realizado no coração do Cariri cearense, no coração do Cariri cearense, o CCSI 2025 também valoriza o território, a cultura local e os desafios específicos da nossa região, promovendo uma formação que vai além do conteúdo técnico: <strong className="text-orange-400 ">integra conhecimento, empatia e compromisso social.</strong>
      
 
         <br />
         <br />
-        Prepare-se para <strong>conectar saberes, ampliar visões e trocar experiências</strong> com quem está na linha de frente do cuidado e da inclusão.
+        Prepare-se para <strong className="text-orange-400">conectar saberes, ampliar visões e trocar experiências</strong> com quem está na linha de frente do cuidado e da inclusão.
 
         <br />
         <br />
